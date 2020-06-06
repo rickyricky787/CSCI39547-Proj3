d4d344948c0afaaca89d857f5ea5da5cffca3ae2
md uddin <mduddin1998@yahoo.com>
2020-06-06 16:46:40 -0400

    final branch update, ready to merge

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-
-
 var numRows = 0;
 var numCols = 0;
 var colorSelected; //undefined by default;
@@ -7,195 +5,178 @@ var isHolding = false;
 
 // Functions
 
-
 //Adds a row
 function addR() {
-    let output = document.getElementById("output"); //get the output to append to it
-    if(numRows === 0){ //create a new row with a new column
-        let newRow = document.createElement("tr"); //create a column
-        let newCol = document.createElement("td");
-        //need to add onclick functionality
-        newCol.onmousedown = function (){
-            this.style.backgroundColor = colorSelected;
-            isHolding = true;
-        };
-        newCol.onmouseup = function()
-        {
-            if (isHolding === true)
-            {
-                isHolding = false;
-            }
-        }
-        newCol.onmouseover = function()
-        {
-            if (isHolding === true)
-            {
-                this.style.backgroundColor = colorSelected;
-            }
-        }
-        newRow.appendChild(newCol);
-        output.appendChild(newRow);
-        numRows++;
-        numCols++;
+  let output = document.getElementById("output"); //get the output to append to it
+  if (numRows === 0) {
+    //create a new row with a new column
+    let newRow = document.createElement("tr"); //create a column
+    let newCol = document.createElement("td");
+    //need to add onclick functionality
+    newCol.onmousedown = function () {
+      this.style.backgroundColor = colorSelected;
+      isHolding = true;
+    };
+    newCol.onmouseup = function () {
+      if (isHolding === true) {
+        isHolding = false;
+      }
+    };
+    newCol.onmouseover = function () {
+      if (isHolding === true) {
+        this.style.backgroundColor = colorSelected;
+      }
+    };
+    newRow.appendChild(newCol);
+    output.appendChild(newRow);
+    numRows++;
+    numCols++;
+  } else {
+    //traverse through the number of rows
+    // let allRows = document.querySelectorAll("tr");
+    let newRow = document.createElement("tr"); //create a column
 
-    }else{ //traverse through the number of rows
-        // let allRows = document.querySelectorAll("tr");
-        let newRow = document.createElement("tr"); //create a column
-
-        for(let i = 0; i<numCols; i++){
-            // console.log("Rorw: ", allRows[i]);
-            let newCol = document.createElement("td");
-            newCol.onmousedown = function (){
-                this.style.backgroundColor = colorSelected;
-                isHolding = true;
-                };
-            newCol.onmouseup = function()
-            {
-                if (isHolding === true)
-                {
-                    isHolding = false;
-                }
-            }
-            newCol.onmouseover = function()
-            {
-                if (isHolding === true)
-                {
-                    this.style.backgroundColor = colorSelected;
-                }
-            }
-            newRow.appendChild(newCol);
-            console.log("done");
+    for (let i = 0; i < numCols; i++) {
+      // console.log("Rorw: ", allRows[i]);
+      let newCol = document.createElement("td");
+      newCol.onmousedown = function () {
+        this.style.backgroundColor = colorSelected;
+        isHolding = true;
+      };
+      newCol.onmouseup = function () {
+        if (isHolding === true) {
+          isHolding = false;
         }
-        output.appendChild(newRow);
-        numRows++;
-        // console.log("the number of rows is: ", numRows, "and cols", numCols);
+      };
+      newCol.onmouseover = function () {
+        if (isHolding === true) {
+          this.style.backgroundColor = colorSelected;
+        }
+      };
+      newRow.appendChild(newCol);
+      console.log("done");
     }
+    output.appendChild(newRow);
+    numRows++;
+    // console.log("the number of rows is: ", numRows, "and cols", numCols);
+  }
 }
 //Adds a column
 function addC() {
-    // console.log("IM in addC");
-    let output = document.getElementById("output"); //get the output to append to it
-    if(numRows === 0){ //create a new row with a new column
-        let newCol = document.createElement("td"); //create a column
-        let newRow = document.createElement("tr");
-        newCol.onmousedown = function (){
-            this.style.backgroundColor = colorSelected;
-            isHolding = true;
-            };
-        newCol.onmouseup = function()
-        {
-            if (isHolding === true)
-            {
-                isHolding = false;
-            }
+  // console.log("IM in addC");
+  let output = document.getElementById("output"); //get the output to append to it
+  if (numRows === 0) {
+    //create a new row with a new column
+    let newCol = document.createElement("td"); //create a column
+    let newRow = document.createElement("tr");
+    newCol.onmousedown = function () {
+      this.style.backgroundColor = colorSelected;
+      isHolding = true;
+    };
+    newCol.onmouseup = function () {
+      if (isHolding === true) {
+        isHolding = false;
+      }
+    };
+    newCol.onmouseover = function () {
+      if (isHolding === true) {
+        this.style.backgroundColor = colorSelected;
+      }
+    };
+    newRow.appendChild(newCol);
+    output.appendChild(newRow);
+    numRows++;
+    numCols++;
+    // console.log("the number of coloms is: ", numCols, "and rows: ", numRows);
+  } else {
+    //traverse through the number of rows
+    let allRows = document.getElementsByTagName("tr");
+    for (let i = 0; i < numRows; i++) {
+      // console.log("Rorw: ", allRows[i]);
+      let newCol = document.createElement("td"); //create a column
+      newCol.onmousedown = function () {
+        this.addEventListener("onmouseup", null);
+        this.style.backgroundColor = colorSelected;
+        isHolding = !isHolding;
+      };
+      newCol.onmouseup = function () {
+        this.removeEventListener("onmouseenter", null);
+        isHolding = isHolding && false;
+      };
+      newCol.onmouseenter = function () {
+        if (isHolding === true) {
+          this.style.backgroundColor = colorSelected;
         }
-        newCol.onmouseover = function()
-        {
-            if (isHolding === true)
-            {
-                this.style.backgroundColor = colorSelected;
-            }
-        }
-        newRow.appendChild(newCol);
-        output.appendChild(newRow);
-        numRows++;
-        numCols++;
-        // console.log("the number of coloms is: ", numCols, "and rows: ", numRows);
-    }else{ //traverse through the number of rows
-        let allRows = document.getElementsByTagName("tr");
-        for(let i = 0; i<numRows; i++){
-            // console.log("Rorw: ", allRows[i]);
-            let newCol = document.createElement("td"); //create a column
-            newCol.onmousedown = function (){
-                this.style.backgroundColor = colorSelected;
-                isHolding = true;
-                };
-            newCol.onmouseup = function()
-            {
-                if (isHolding === true)
-                {
-                    isHolding = false;
-                }
-            }
-            newCol.onmouseover = function()
-            {
-                if (isHolding === true)
-                {
-                    this.style.backgroundColor = colorSelected;
-                }
-            }
-            allRows[i].appendChild(newCol);
-        }
-        numCols++;
-        // console.log("the number of coloms is: ", numCols, "num of rows: ", numRows);
-
+      };
+      allRows[i].appendChild(newCol);
     }
+    numCols++;
+    // console.log("the number of coloms is: ", numCols, "num of rows: ", numRows);
+  }
 }
 
 //Removes a row
 function removeR() {
-    let output = document.getElementById("output")
-    if((numCols === 0) || (numRows===0)){
-        alert("There is nothing to delete bruh!!!");
-    }
-    else{
-        let output = document.getElementById("output");
-        let lastRow = output.lastElementChild;
-        output.removeChild(lastRow);
-        numRows--;
-        if(numRows===0) numCols = 0;
-    }
-    console.log("the number of coloms is: ", numCols, "num of rows: ", numRows);
+  let output = document.getElementById("output");
+  if (numCols === 0 || numRows === 0) {
+    alert("There is nothing to delete bruh!!!");
+  } else {
+    let output = document.getElementById("output");
+    let lastRow = output.lastElementChild;
+    output.removeChild(lastRow);
+    numRows--;
+    if (numRows === 0) numCols = 0;
+  }
+  console.log("the number of coloms is: ", numCols, "num of rows: ", numRows);
 }
 //Remove a column
 function removeC() {
-    let output = document.getElementById("output")
-    if((numCols === 0) || (numRows===0)){
-        alert("There is nothing to delete bruh!!!");
+  let output = document.getElementById("output");
+  if (numCols === 0 || numRows === 0) {
+    alert("There is nothing to delete bruh!!!");
+  } else {
+    // let output = document.getElementById("output");
+    let allRows = document.getElementsByTagName("tr");
+    for (let i = 0; i < numRows; i++) {
+      // console.log("Rorw: ", allRows[i]);
+      let rCol = allRows[i].lastElementChild; //create a column
+      allRows[i].removeChild(rCol);
     }
-    else{
-        // let output = document.getElementById("output");
-        let allRows = document.getElementsByTagName("tr");
-        for(let i = 0; i<numRows; i++){
-            // console.log("Rorw: ", allRows[i]);
-            let rCol = allRows[i].lastElementChild; //create a column
-            allRows[i].removeChild(rCol);
-        }
-        numCols--;
-        if(numCols===0) numRows = 0;
-    }
-    console.log("the number of coloms is: ", numCols, "num of rows: ", numRows);
+    numCols--;
+    if (numCols === 0) numRows = 0;
+  }
+  console.log("the number of coloms is: ", numCols, "num of rows: ", numRows);
 }
 //sets global var for selected color
-function selected(){
-    colorSelected = document.getElementById("colors").value;
-    console.log(colorSelected);
+function selected() {
+  colorSelected = document.getElementById("colors").value;
+  console.log(colorSelected);
 }
 
-function fill(){
-    var table = document.getElementById("output");
-    for (var i = 0, row; row = table.rows[i]; i++) {
-        for (var j = 0, col; col = row.cells[j]; j++) {
-                col.style.backgroundColor=colorSelected;
-        }
+function fill() {
+  var table = document.getElementById("output");
+  for (var i = 0, row; (row = table.rows[i]); i++) {
+    for (var j = 0, col; (col = row.cells[j]); j++) {
+      col.style.backgroundColor = colorSelected;
     }
+  }
 }
-function clearAll(){
-    var table = document.getElementById("output");
-    for (var i = 0, row; row = table.rows[i]; i++) {
-        for (var j = 0, col; col = row.cells[j]; j++) {
-                col.style.backgroundColor="";
-        }
+function clearAll() {
+  var table = document.getElementById("output");
+  for (var i = 0, row; (row = table.rows[i]); i++) {
+    for (var j = 0, col; (col = row.cells[j]); j++) {
+      col.style.backgroundColor = "";
     }
+  }
 }
 
-function fillU(){
-    var table = document.getElementById("output");
-    for (var i = 0, row; row = table.rows[i]; i++) {
-        for (var j = 0, col; col = row.cells[j]; j++) {
-            if(col.style.backgroundColor===""){
-                col.style.backgroundColor=colorSelected;
-            }
-        }
+function fillU() {
+  var table = document.getElementById("output");
+  for (var i = 0, row; (row = table.rows[i]); i++) {
+    for (var j = 0, col; (col = row.cells[j]); j++) {
+      if (col.style.backgroundColor === "") {
+        col.style.backgroundColor = colorSelected;
+      }
     }
+  }
 }
